715e953937c25b3a719cfbc18d3573e752ab6467
fatihbguclu <curaat79@gmail.com>
2024-10-27 12:21:39 +0300

    Junit test throw and timeouts

---
+++ UnitTest1.00/src/test/java/org/ft/demoUtilsTestThrowAndTimeOuts.java
@@ -0,0 +1,34 @@
+package org.ft;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class demoUtilsTestThrowAndTimeOuts {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+    }
+
+    @DisplayName("Throws and Does Not Throw")
+    @Test
+    void testThrowsAndDoesNotThrow() {
+        assertThrows(Exception.class, () -> demoUtils.throwException(-1), "Should throw exception");
+        assertDoesNotThrow(() -> demoUtils.throwException(5), "Should not throw exception");
+    }
+
+    @DisplayName("Timeout")
+    @Test
+    void testTimeout() {
+        assertTimeoutPreemptively(Duration.ofSeconds(3), () -> demoUtils.checkTimeout(),
+                "Method should execute in 3 seconds");
+    }
+
+}
