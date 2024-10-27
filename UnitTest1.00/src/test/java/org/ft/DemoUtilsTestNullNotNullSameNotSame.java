97cd11abada37448c8963130f8d5b394f1f80392
fatihbguclu <curaat79@gmail.com>
2024-10-27 11:46:00 +0300

    Junit null, NotNull, Equals, NotEquals, Same, NotSame assertions

---
+++ UnitTest1.00/src/test/java/org/ft/DemoUtilsTestNullNotNullSameNotSame.java
@@ -0,0 +1,57 @@
+package org.ft;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoUtilsTestNullNotNullSameNotSame {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+    }
+
+    @Test
+    @DisplayName("Equals and Not Equals")
+    void testEqualsAndNotEquals() {
+
+        assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
+    }
+
+    @Test
+    @DisplayName("Null and Not Null")
+    void testNullAndNotNull() {
+
+        String str1 = null;
+        String str2 = "luv2code";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+
+    }
+
+    @DisplayName("Same and Not Same")
+    @Test
+    void testSameAndNotSame() {
+
+        String str = "luv2code";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to same object");
+        assertNotSame(str, demoUtils.getAcademy(), "Objects should not refer to same object");
+    }
+
+    @DisplayName("True and False")
+    @Test
+    void testTrueFalse() {
+        int gradeOne = 10;
+        int gradeTwo = 5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+
+    }
+
+}
