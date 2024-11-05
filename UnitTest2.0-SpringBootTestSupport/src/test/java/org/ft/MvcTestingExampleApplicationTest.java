c8d86e913284663f4d16233e72b99d5fd689340e
fatihbguclu <curaat79@gmail.com>
2024-11-05 22:24:14 +0300

    Spring Boot Test Support

---
+++ UnitTest2.0-SpringBootTestSupport/src/test/java/org/ft/MvcTestingExampleApplicationTest.java
@@ -0,0 +1,10 @@
+package org.ft;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+class MvcTestingExampleApplicationTest {
+
+}
