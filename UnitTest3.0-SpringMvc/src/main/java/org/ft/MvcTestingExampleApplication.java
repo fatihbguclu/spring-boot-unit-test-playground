68c214fa5cf10931897ec59af5d289634fe9ac0f
fatihbguclu <curaat79@gmail.com>
2024-11-09 21:24:54 +0300

    add new module for testing Spring Mvc application

---
+++ UnitTest3.0-SpringMvc/src/main/java/org/ft/MvcTestingExampleApplication.java
@@ -0,0 +1,13 @@
+package org.ft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcTestingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcTestingExampleApplication.class, args);
+    }
+
+}
