22fbc8e4f73807cb364564c0892530849d24068a
fatihbguclu <curaat79@gmail.com>
2024-11-10 15:37:57 +0300

    test mvc controller

--- UnitTest3.0-SpringMvc/src/main/java/org/ft/controller/GradebookController.java
+++ UnitTest3.0-SpringMvc/src/main/java/org/ft/controller/GradebookController.java
@@ -1,6 +1,7 @@
 package org.ft.controller;
 
 import org.ft.model.Gradebook;
+import org.ft.service.StudentAndGradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,8 +16,12 @@ public class GradebookController {
     @Autowired
     private Gradebook gradebook;
 
+    @Autowired
+    private StudentAndGradeService studentAndGradeService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getStudents(Model m) {
+        m.addAttribute("students", studentAndGradeService.getGradeBook());
         return "index";
     }
 
---
+++ UnitTest3.0-SpringMvc/src/test/java/org/ft/GradebookControllerTest.java
@@ -0,0 +1,70 @@
+package org.ft;
+
+import org.ft.model.CollegeStudent;
+import org.ft.model.GradebookCollegeStudent;
+import org.ft.service.StudentAndGradeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@TestPropertySource("/application.properties")
+@AutoConfigureMockMvc
+public class GradebookControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private StudentAndGradeService studentCreateServiceMock;
+
+    @BeforeEach
+    public void beforeEach() {
+        jdbc.execute("insert into student(id, firstname, lastname, email_address) " + "values (1, 'Eric', 'Roby', 'eric.roby@luv2code_school.com')");
+    }
+
+    @AfterEach
+    public void setupAfterTransaction() {
+        jdbc.execute("DELETE FROM student");
+    }
+
+    @Test
+    public void getStudentsHttpRequest() throws Exception{
+        CollegeStudent studentOne = new GradebookCollegeStudent("Eric", "Roby", "eric_roby@luv2code_school.com");
+        CollegeStudent studentTwo = new GradebookCollegeStudent("Chad", "Darby", "chad_darby@luv2code_school.com");
+        List<CollegeStudent> collegeStudentList = new ArrayList<>(Arrays.asList(studentOne,studentTwo));
+
+        when(studentCreateServiceMock.getGradeBook()).thenReturn(collegeStudentList);
+        assertIterableEquals(collegeStudentList, studentCreateServiceMock.getGradeBook());
+
+        MvcResult mvcResult = mockMvc
+                .perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ModelAndView modelAndView = mvcResult.getModelAndView();
+        ModelAndViewAssert.assertViewName(modelAndView, "index");
+    }
+}
